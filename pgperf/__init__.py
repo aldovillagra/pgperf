4edd7628cc11cc9999bc66d467ffeb27d3a2c8e1
Aldo A. Villagra B <Aldo A. Villagra B>
2022-06-21 16:34:46 +0200

    ADD doc and readme

--- pgperf/__init__.py
+++ pgperf/__init__.py
@@ -2,14 +2,35 @@ __version__ = '0.1.0'
 
 import fire
 import os
-import sys
 from rich.console import Console
 from omegaconf import OmegaConf
 from jinja2 import Environment, PackageLoader
 
 console = Console()
 user_home = os.path.expanduser('~')
-config = OmegaConf.load(user_home + '/.pgperf.yml')
+default_config = user_home + '/.pgperf.yml'
+try:
+    OmegaConf.load(default_config)
+except Exception:
+    console.print(f"[white] Creating default config...")
+    conf_yaml = """
+prod:
+  db:
+    server: 127.0.0.1
+    port: 5432
+    db: prod_db_name
+    user: prod_db_user
+    password: prod_db_pass
+    """
+    conf = OmegaConf.create(conf_yaml)
+    with open(default_config, 'w') as file:
+        OmegaConf.save(config=conf, f=file)
+        file.flush()
+    console.print(
+        f"[bold yellow] Please edit in => [bold green]{default_config}")
+    console.print("[white] Enter for continue... ")
+    input()
+config = OmegaConf.load(default_config)
 file_loader = PackageLoader('pgperf', 'templates')
 env = Environment(loader=file_loader)
 env.trim_blocks = True
